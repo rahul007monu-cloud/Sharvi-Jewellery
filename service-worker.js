64ed27d510a92027b4bb3cd3e1e292dba687f1fb
rahul007monu-cloud <rahul007monu@gmail.com>
2026-05-28 13:46:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -82,3 +82,12 @@ function syncOrders() {
   // Sync orders when back online
   return Promise.resolve();
 }
+<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered'))
+      .catch(err => console.log('SW error:', err));
+  });
+}
+</script>
